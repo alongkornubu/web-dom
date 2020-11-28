bf77919e214e0c80f5af2ea8fca18d9483cf936f
Alongkorn Chaichana <62642030+alongkornubu@users.noreply.github.com>
2020-11-28 17:30:29 +0700

    Add files via upload
    changed

---
+++ script.js
@@ -0,0 +1,18 @@
+const fetchCat = () => {
+    //Add two lines
+    let loader = `<div class=”lds-ellipsis”><div></div><div></div><div></div><div></div></div>`;
+    document.getElementById('cat-pic').innerHTML = loader;
+    //Add two lines
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then(res => res.json())
+      .then(cats => {
+          console.log(cats);
+          cats.forEach(cat => {
+          let result = `<h2>Here your lovely cat </h2>
+              <div>
+                  <a href="${cat.url}" target="_blank"><img src="${cat.url}"></a>
+              </div>`;
+          document.getElementById('cat-pic').innerHTML = result;
+          })
+    })
+};
